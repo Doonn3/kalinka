e1534e431b97e42cae9b034a288d68de0f08daa8
Doonn <Doonn3@gmail.com>
2024-06-08 15:19:55 +0300

    fear: add json server, db, layouts and global style

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { lang, switchLanguage } from "~/stores/AppLanguage/store.language";
+</script>
+
+<template>
+  <div>
+    <SwitchLanguge
+      class="btn-language"
+      :lang="lang"
+      @emit:switch="switchLanguage"
+    />
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.btn-language {
+  position: absolute;
+  right: 10px;
+  bottom: 10px;
+}
+</style>
