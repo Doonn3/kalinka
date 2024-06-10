d59ed317dd092ba1cc97bd4403384f61d6de90a2
Doonn <Doonn3@gmail.com>
2024-06-10 11:38:01 +0300

    refactor: delete

--- src/server/assets/index.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "nuxt/app";
-
-export default defineEventHandler(async () => {
-  const assets = useState("assets:server");
-  const db = await assets.getItem("db.json");
-  return {
-    db,
-  };
-});
